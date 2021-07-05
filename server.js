ea62317b58783f80fe9210650193ff34938b0f80
Ana Claudia <ananutri6@gmail.com>
2021-07-05 13:46:48 -0300

    alterações-receitas-saudáveis

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+//conectar database
+
+//usar as rotas
+
+app.use(express.json())
+
+
+app.listen(3333, ()=> console.log('Servidor rodando!'))
+
+
+
